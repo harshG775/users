aa3ccfc09badbe700da110b362180760eea077e5
Harsh <hgaur491@gmail.com>
2023-09-27 16:34:58 +0530

    //////////////////////////////

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const mongoose = require('mongoose');
 dotenv.config({path:`${__dirname}/config.env`})
 const app = require("./app");
 
-const DB = process.env.CLOUD_DB.replace("<password>",process.env.API_DB_PASS)
-const localDB = process.env.LOCAL_DB
-mongoose.connect(localDB+"/test_api")
+let DB = process.env.CLOUD_DB.replace("<password>",process.env.API_DB_PASS)
+if (process.env.NODE_ENV==="development") {
+    DB = process.env.LOCAL_DB
+}
+console.log(DB)
+mongoose.connect(DB+"/test_api")
 	.then(() => {console.log('mongoose is connected !!!!!!')})
 	.catch((err) => {console.log('\x1b[1;31m oh no error !!!!\n \n \x1b[0m', err)});
 module.exports = {mongoose}
