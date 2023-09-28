fd4612e11f34252a038f8d948dd2463fb416d23e
Harsh <hgaur491@gmail.com>
2023-09-28 17:01:23 +0530

    fixing script

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ dotenv.config({path:`${__dirname}/config.env`})
 const app = require("./app");
 
 let DB = process.env.CLOUD_DB.replace("<password>",process.env.API_DB_PASS)
-// if (process.env.NODE_ENV==="development") {
-//     DB = process.env.LOCAL_DB
-// }
-
+if (process.env.NODE_ENV != "production") {
+    DB = process.env.LOCAL_DB
+}
 
 mongoose.connect(DB+"/test_api")
 	.then(() => {console.log('mongoose is connected !!!!!!')})
