18a27b78d1bfc480379c98add1d62f12f5d1ae05
Harsh <hgaur491@gmail.com>
2023-09-28 13:28:48 +0530

    commenting local env if else

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ dotenv.config({path:`${__dirname}/config.env`})
 const app = require("./app");
 
 let DB = process.env.CLOUD_DB.replace("<password>",process.env.API_DB_PASS)
-if (process.env.NODE_ENV==="development") {
-    DB = process.env.LOCAL_DB
-}
-console.log(DB)
+// if (process.env.NODE_ENV==="development") {
+//     DB = process.env.LOCAL_DB
+// }
+
+
 mongoose.connect(DB+"/test_api")
 	.then(() => {console.log('mongoose is connected !!!!!!')})
 	.catch((err) => {console.log('\x1b[1;31m oh no error !!!!\n \n \x1b[0m', err)});
