383846d14c508b8520948022bee3c1a3ce9caae8
Harsh <hgaur491@gmail.com>
2023-10-09 18:38:56 +0530

    some fixes

--- app.js
+++ app.js
@@ -13,6 +13,31 @@ app.use(express.static(__dirname+"/frontend"))
 app.use(express.json());
 
 
+/* API ROUTES */
 app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/courses", coursesRouter);
+
+
+/* GLOBAL ROUTE */
+app.all("*", (req,res,next)=>{
+    // res.status(404).json({
+    //     status:"fail",
+    //     message:`can not find the endpoint: ${req.originalUrl}`
+    // })
+    const err = new Error(`can not find the endpoint: ${req.originalUrl}`)
+    err.status = "fail"
+    err.statusCode = 404
+    next(err);
+});
+
+app.use((err ,req,res,next)=>{
+    err.statusCode = err.statusCode || 500
+    err.status =err.status || "error"
+    res.status(err.statusCode).json({
+        statusCode:err.statusCode,
+        status:err.status,
+        message:err.message
+    })
+})
+
 module.exports = app
